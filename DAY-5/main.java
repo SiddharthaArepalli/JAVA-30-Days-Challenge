dc1c2b9fa5ba533fc5c9b9c1aa52b1acf2ed5546
SiddharthaArepalli <140429954+SiddharthaArepalli@users.noreply.github.com>
2023-11-05 21:50:57 +0530

    Add files via upload

---
+++ DAY-5/main.java
@@ -0,0 +1,31 @@
+    
+    // Strings // 
+    public class main {
+        public static void main(String[] args) {
+          String greeting = "Hello";
+          System.out.println(greeting);
+
+          /// string Length :
+
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The length of the txt string is: " + txt.length());
+
+              // more string handling functions 
+
+        String con = "Hello World";
+    System.out.println(con.toUpperCase());
+    System.out.println(con.toLowerCase());
+            // 2 string function
+
+            String tot = "Please locate where 'locate' occurs!";
+System.out.println(tot.indexOf("locate")); // Outputs 7
+    }
+  }
+      
+
+      
+
+      
+  
+
+
