9b8ab030521162b575524598c342647059c852de
SiddharthaArepalli <140429954+SiddharthaArepalli@users.noreply.github.com>
2023-11-06 22:12:06 +0530

    Add files via upload

---
+++ DAY-6/concat.java
@@ -0,0 +1,14 @@
+public class concat {
+  public static void main(String[] args) {
+    String firstName = "John ";
+    String lastName = "Doe";
+    System.out.println(firstName.concat(lastName));
+
+    // using concat()
+    String firstN = "John ";
+    String lastN = "Doe";
+    System.out.println(firstN.concat(lastN));
+  }
+}
+
+
